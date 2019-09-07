d94963ececb9ece76a1820d4f4003d173b37f06b
unknown <dvreimer@calpoly.edu>
2019-09-07 14:31:51 -0700

    Added Candidate struct to Election smart contract

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
     Tutorial Notes:
         -Source: http://www.dappuniversity.com/articles/the-ultimate-ethereum-dapp-tutorial
-        -Constructor function deprecated, use "constructor()" rather than "function ContractName()"
+        -Constructor function deprecated, use "constructor()"   rather than "function ContractName()"
+        -Must add data location for function variables, "memory" to store temporary values and "storage" to store persistent contract state variables
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,23 @@
 pragma solidity >=0.4.2;
 
 contract Election {
-    string public candidate;
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
     }
 }

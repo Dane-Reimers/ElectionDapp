6e0aa0d8aaf78bec5eee96f7142038e3b795e3a2
unknown <dvreimer@calpoly.edu>
2019-09-07 15:30:15 -0700

    Added voting functionality to Election contract

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
         -Source: http://www.dappuniversity.com/articles/the-ultimate-ethereum-dapp-tutorial
         -prepend version of pragma solidity line with ">="
         -Constructor function deprecated, use "constructor()"   rather than "function ContractName()"
-        -Must add data location for function variables, "memory" to store temporary values and "storage" to store persistent contract state variables
+        -Must add data location for string function variables, "memory" to store temporary values and "storage" to store persistent contract state variables
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,7 @@ contract Election {
         uint voteCount;
     }
 
+    mapping(address => bool) public voters;
     mapping(uint => Candidate) public candidates;
     uint public candidatesCount;
 
@@ -19,5 +20,14 @@ contract Election {
     function addCandidate(string memory name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
+    }
+
+    function vote(uint candidateId) public {
+        require(!voters[msg.sender], "Cannot have already voted");
+        require(candidateId > 0 && candidateId <= candidatesCount,
+            "Must vote for valid candidate");
+
+        voters[msg.sender] = true;
+        candidates[candidateId].voteCount++;
     }
 }
